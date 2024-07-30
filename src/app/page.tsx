3c7128f4e3ed7e4bf48209ba79cb67351e5d87f3
ManuelVerutti <79605813+ManuelVerutti@users.noreply.github.com>
2024-07-29 20:03:12 -0500

    add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import Image from "next/image";
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-12 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Bienvenido</h1>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Esta es la prueba técnica de Manuel Verutti para Intercall
+          </p>
+        </div>
+        <div className="mt-8 space-y-4">
+          <Link href="/products" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            Productos
+          </Link>
+          <Link href="/sales" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+            Ventas
+          </Link>
+          <Link href="/users" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+            Usuarios
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
